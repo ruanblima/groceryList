19e16bf956ac71322778366a23334292b4184d6c
Ruan Lima <ruan.lima@dellead.com>
2021-11-25 09:18:43 -0300

    Creating logic login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,18 +1,14 @@
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
 import Input from '~/components/Input';
 
 import groceries from '~/assets/images/groceries.png';
-import { HOME_SCREEN } from '~/constants/routes';
 import { loginAction } from '~/store/ducks/user/actions';
 
 import * as S from './styles';
 
-type Props = NativeStackScreenProps<any, 'LoginScreen'>;
-
-export function Login({ navigation }: Props) {
+export function Login() {
   const dispatch = useDispatch();
 
   const [username, setUsername] = useState('');
@@ -21,7 +17,6 @@ export function Login({ navigation }: Props) {
 
   function handleLogin() {
     dispatch(loginAction(username, password));
-    navigation.navigate(HOME_SCREEN);
   }
 
   return (
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { useSelector } from 'react-redux';
 import { ThemeProvider } from 'styled-components/native';
 
 import { Cart } from '~/pages/Cart';
@@ -11,6 +12,7 @@ import { NewItem } from '~/pages/NewItem';
 
 import { Header } from '~/components/Header';
 
+import type { AplicationState } from '~/@types/entities/AplicationState';
 import {
   HOME_SCREEN,
   LOGIN_SCREEN,
@@ -20,48 +22,55 @@ import {
 import Theme from '~/themes';
 
 const Stack = createNativeStackNavigator();
+const StackLogin = createNativeStackNavigator();
 
 export function RootStack() {
+  const { isLogged } = useSelector((state: AplicationState) => state.user);
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
       <ThemeProvider theme={Theme.light}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName={LOGIN_SCREEN}>
-            <Stack.Screen
-              name={LOGIN_SCREEN}
-              component={Login}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name={HOME_SCREEN}
-              component={Home}
-              options={{
-                header: props => <Header {...props} />,
-              }}
-            />
-            <Stack.Screen
-              name={CART_SCREEN}
-              component={Cart}
-              options={{
-                header: props => (
-                  <Header enableNavigation title="Carrinho" {...props} />
-                ),
-              }}
-            />
-            <Stack.Screen
-              name={NEW_ITEM_SCREEN}
-              component={NewItem}
-              options={{
-                header: props => (
-                  <Header
-                    enableNavigation
-                    title="Cadastrar Produto"
-                    {...props}
-                  />
-                ),
-              }}
-            />
-          </Stack.Navigator>
+          {isLogged ? (
+            <Stack.Navigator initialRouteName={HOME_SCREEN}>
+              <Stack.Screen
+                name={HOME_SCREEN}
+                component={Home}
+                options={{
+                  header: props => <Header {...props} />,
+                }}
+              />
+              <Stack.Screen
+                name={CART_SCREEN}
+                component={Cart}
+                options={{
+                  header: props => (
+                    <Header enableNavigation title="Carrinho" {...props} />
+                  ),
+                }}
+              />
+              <Stack.Screen
+                name={NEW_ITEM_SCREEN}
+                component={NewItem}
+                options={{
+                  header: props => (
+                    <Header
+                      enableNavigation
+                      title="Cadastrar Produto"
+                      {...props}
+                    />
+                  ),
+                }}
+              />
+            </Stack.Navigator>
+          ) : (
+            <StackLogin.Navigator initialRouteName={LOGIN_SCREEN}>
+              <StackLogin.Screen
+                name={LOGIN_SCREEN}
+                component={Login}
+                options={{ headerShown: false }}
+              />
+            </StackLogin.Navigator>
+          )}
         </NavigationContainer>
       </ThemeProvider>
     </SafeAreaView>
