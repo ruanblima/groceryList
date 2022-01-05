11d7b2a29b51c5be28a03a44994638938141a96f
Ruan Lima <ruan.lima@dellead.com>
2022-01-05 11:42:27 -0300

    Edit  file responsible text

--- src/utils/responsibleText/index.ts
+++ src/utils/responsibleText/index.ts
@@ -1,23 +1,5 @@
-import { Platform, StatusBar, Dimensions } from 'react-native';
+import { Dimensions } from 'react-native';
 import { moderateScale } from 'react-native-size-matters';
-
-const { height, width } = Dimensions.get('window');
-const standardLength = width > height ? width : height;
-const offset = width > height ? 0 : StatusBar.currentHeight;
-
-const deviceHeight =
-  Platform.OS === 'android' ? standardLength - offset : standardLength;
-
-export function RFPercentage(percent: any) {
-  const heightPercent = (percent * deviceHeight) / 100;
-  return Math.round(heightPercent);
-}
-
-// guideline height for standard 5" device screen is 680
-export function RFValue(fontSize: number, standardScreenHeight = 680) {
-  const heightPercent = (fontSize * deviceHeight) / standardScreenHeight;
-  return Math.round(heightPercent);
-}
 
 const vh = Dimensions.get('window').height / 100;
 const vw = Dimensions.get('window').width / 100;
